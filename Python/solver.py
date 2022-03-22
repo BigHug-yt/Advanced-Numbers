bea13d78ea29304029ebef033e0d0fa1a5fcdd85
BigHug <81681958+BigHug-yt@users.noreply.github.com>
2022-03-22 14:10:23 +0000

    added some C++ options

--- Python/solver.py
+++ Python/solver.py
@@ -1,6 +1,6 @@
 from numbersImpl import num, expression
 
-def Neg(arg):
+def Neg(arg, lvl):
 	if arg.Type == "NUMB":
 		if arg.Args[0].Type == "INT" or arg.Args[0].Type == "FLOAT" or arg.Args[0].Type == "ANGLE" or arg.Args[0].Type == "RAD":
 			return expression("NUMB", [num(arg.Args[0].Type, -arg.Args[0].Val)])
@@ -8,7 +8,7 @@ def Neg(arg):
 	if arg.Type == "NEG":
 		return arg.Args[0]
 
-def Abs(arg):
+def Abs(arg, lvl):
 	if arg.Type == "NUMB":
 		if arg.Args[0].Type == "INT" or arg.Args[0].Type == "FLOAT" or arg.Args[0].Type == "ANGLE" or arg.Args[0].Type == "RAD":
 			return expression("NUMB", [num(arg.Args[0].Type, abs(arg.Args[0].Val))])
@@ -16,7 +16,7 @@ def Abs(arg):
 	if arg.Type == "ABS":
 		return expression("ABS", [arg.Args[0]])
 
-def Add(arg1, arg2):
+def Add(arg1, arg2, lvl):
 	if arg1.Type == "NUMB":
 		if arg2.Type == "NUMB":
 			if arg1.Args[0].Type == "INT" and arg2.Args[0].Type == "INT":
@@ -32,11 +32,13 @@ def Add(arg1, arg2):
 			if arg1.Args[0].Type == "RAD" and arg2.Args[0].Type == "RAD":
 				return expression("NUMB", [num("RAD", arg1.Args[0].Val+arg2.Args[0].Val)])
 
+		if arg2.Type == "NEG":
+			
 			raise Exception(f"adding numbers with types {arg1.Args[0].Type} and {arg2.Args[0].Type} is not (yet) supported")
 		raise Exception(f"adding {arg2.Type} to NUMB is not (yet) supported")
 	raise Exception(f"adding to {arg1.Type} is not yet supported")
 			
-def solve(expr):
+def solve(expr, lvl=0):
 	if type(expr) == num:
 		return expression("NUMB", [expr])
 	if type(expr) == expression:
@@ -44,15 +46,18 @@ def solve(expr):
 			return expr
 		if expr.Type == "NEG":
 			arg = solve(expr.Args[0])
-			return Neg(arg)
+			return Neg(arg, lvl)
 		if expr.Type == "ABS":
 			arg = solve(expr.Args[0])
-			return Abs(arg)
+			return Abs(arg, lvl)
 		if expr.Type == "ADD":
-			# only works for 2 args for now!!!
-			arg1 = solve(expr.Args[0])
-			arg2 = solve(expr.Args[1])
-			return Add(arg1, arg2)
+			prevarg = 0
+			for arg in expr.Args:
+				if prevarg == 0:
+					prevarg = solve(arg, lvl)
+				else:
+					prevarg = Add(solve(prevarg, lvl), solve(arg, lvl), lvl)
+			return solve(prevarg, lvl)
 
 		raise Exception(f"solving an expression of type {expr.Type} is not (yet) implemented")
 
